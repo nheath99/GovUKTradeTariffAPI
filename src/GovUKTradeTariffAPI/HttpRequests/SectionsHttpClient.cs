34fdfe538bc9ea07a85ccd8c89196f673753343b
Nick Heath <nick@tapiit.com>
2020-01-05 20:30:30 +0000

    Added an example http client. Need to see if this is a suitable approach.

---
+++ src/GovUKTradeTariffAPI/HttpRequests/SectionsHttpClient.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GovUKTradeTariffAPI.HttpRequests
+{
+	public class SectionsHttpClient
+	{
+		public SectionsHttpClient(HttpClient httpClient)
+		{
+			client = httpClient;
+		}
+
+		private readonly HttpClient client;
+
+		public async Task<Sections> ListAsync()
+		{
+			var sections = await client.GetAsync("sections");
+
+			if (sections.IsSuccessStatusCode)
+			{
+				var json = await sections.Content.ReadAsStringAsync();
+				var obj = JsonConvert.DeserializeObject<Sections>(json);
+
+				return obj;
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		public async Task<Section> ListAsync(int position)
+		{
+			var sections = await client.GetAsync($"sections/{position}");
+
+			if (sections.IsSuccessStatusCode)
+			{
+				var json = await sections.Content.ReadAsStringAsync();
+				var obj = JsonConvert.DeserializeObject<Section>(json);
+
+				return obj;
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		public async Task<SectionNote> NoteAsync(int position)
+		{
+			var sections = await client.GetAsync($"sections/{position}/section_note");
+
+			if (sections.IsSuccessStatusCode)
+			{
+				var json = await sections.Content.ReadAsStringAsync();
+				var obj = JsonConvert.DeserializeObject<SectionNote>(json);
+
+				return obj;
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+}
---
+++ src/GovUKTradeTariffAPI/TradeTariffAPIClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using GovUKTradeTariffAPI.HttpRequests;
+
+namespace GovUKTradeTariffAPI
+{
+	public class TradeTariffApiClient
+	{
+		public TradeTariffApiClient(IHttpClientFactory httpClientFactory)
+		{
+			HttpClient = httpClientFactory.CreateClient("tradeTariffApiClient");
+			HttpClient.BaseAddress = new Uri($"{BaseUrl}/{Version}/");
+		}
+
+		public const string BaseUrl = "https://www.trade-tariff.service.gov.uk/api";
+		public const string Version = "v2";
+
+		internal HttpClient HttpClient { get; private set; }
+
+		public SectionsHttpClient Sections { get; set; }
+	}
+}
